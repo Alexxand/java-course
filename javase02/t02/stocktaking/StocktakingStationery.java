1b0daac3539be7981c8707310234f9eeb77aeb5c
Alexandr <alexmich@yandex.ru>
2016-10-13 16:36:46 +0300

    Done javase01.t02 using TDD, stream and method reference

---
+++ javase02/t02/stocktaking/StocktakingStationery.java
@@ -0,0 +1,31 @@
+package javase02.t02.stocktaking;
+
+
+import javase02.t03.stationery.Stationery;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StocktakingStationery {
+    private List<Stationery> stationeryList = new ArrayList<>();
+
+    public boolean isNothing(){
+        return true;
+    }
+
+    public boolean add(String name, int cost){
+        return stationeryList.add(new Stationery(name,cost));
+    }
+
+    public boolean isThere(String name, int cost){
+        return stationeryList.contains(new Stationery(name,cost));
+    }
+
+    public boolean remove(String name, int cost) {
+        return stationeryList.remove(new Stationery(name,cost));
+    }
+
+    public int totalCost(){
+        return stationeryList.stream().mapToInt(Stationery::getCost).sum();
+    }
+}
