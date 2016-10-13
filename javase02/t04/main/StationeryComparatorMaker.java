139c29f7990f0598ecc9c318cd387699c7c9704e
Alexandr <alexmich@yandex.ru>
2016-10-14 01:36:51 +0300

    Changed task4 with comparing and thenComparing

---
+++ javase02/t04/main/StationeryComparatorMaker.java
@@ -0,0 +1,30 @@
+package javase02.t04.main;
+
+import javase02.t03.stationery.Stationery;
+
+import java.util.Comparator;
+
+/**
+ * Created by alexmich on 11.10.16.
+ */
+public class StationeryComparatorMaker{
+
+
+    public static Comparator<Stationery> make(Order order) {
+        switch(order){
+            default:
+            case BY_NAME:
+                return Comparator.comparing(Stationery::getName);
+            case BY_COST:
+                return Comparator.comparing(Stationery::getCost);
+            case BY_COST_THEN_BY_NAME:
+                return Comparator.comparing(Stationery::getCost).thenComparing(Stationery::getName);
+        }
+    }
+
+    public enum Order{
+        BY_NAME,
+        BY_COST,
+        BY_COST_THEN_BY_NAME
+    }
+}
--- javase02/t04/test/StationeryComparatorMakerTest.java
+++ javase02/t04/test/StationeryComparatorMakerTest.java
@@ -3,17 +3,18 @@ package javase02.t04.test;
 
 import javase02.t03.set.BeginnerSetMaker;
 import javase02.t03.stationery.Stationery;
-import javase02.t04.comparators.CostStationeryComparator;
-import javase02.t04.comparators.CostNameStationeryComparator;
-import javase02.t04.comparators.NameStationeryComparator;
+import javase02.t04.main.StationeryComparatorMaker;
 import org.junit.Test;
 
+import java.util.Comparator;
 import java.util.List;
+
+import static javase02.t04.main.StationeryComparatorMaker.Order.*;
 
 /**
  * Created by alexmich on 11.10.16.
  */
-public class ComparatorsTest {
+public class StationeryComparatorMakerTest {
 
     private void print(List<? extends Stationery> set){
         for (Stationery elem: set) {
@@ -23,38 +24,38 @@ public class ComparatorsTest {
     }
 
     @Test
-    public void byCostTest(){
+    public void byCost(){
         BeginnerSetMaker setMaker = new BeginnerSetMaker();
         List<Stationery> set = setMaker.make();
-        System.out.println("byCostTest");
+        System.out.println("byCost");
         System.out.println("Source set:");
         print(set);
-        set.sort(new CostStationeryComparator());
+        set.sort(StationeryComparatorMaker.make(BY_COST));
         System.out.println("Modified set:");
         print(set);
 
     }
 
     @Test
-    public void byNameTest(){
+    public void byName(){
         BeginnerSetMaker setMaker = new BeginnerSetMaker();
         List<Stationery> set = setMaker.make();
-        System.out.println("byNameTest");
+        System.out.println("byName");
         System.out.println("Source set:");
         print(set);
-        set.sort(new NameStationeryComparator());
+        set.sort(StationeryComparatorMaker.make(BY_NAME));
         System.out.println("Modified set:");
         print(set);
     }
 
     @Test
-    public void byNameCostTest(){
+    public void byCostThenByName(){
         BeginnerSetMaker setMaker = new BeginnerSetMaker();
         List<Stationery> set = setMaker.make();
-        System.out.println("byNameCostTest");
+        System.out.println("byCostThenByName");
         System.out.println("Source set:");
         print(set);
-        set.sort(new CostNameStationeryComparator());
+        set.sort(StationeryComparatorMaker.make(BY_COST_THEN_BY_NAME));
         System.out.println("Modified set:");
         print(set);
 
