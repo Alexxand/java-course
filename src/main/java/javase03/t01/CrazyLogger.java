a595a1195d3f636742ea4150a9a42d78450e42e2
Alexandr <alexmich@yandex.ru>
2016-10-24 01:52:07 +0300

    Added javadoc to CrazyLogger and removed thrown IOException from CrazyLogger::print(PrintStream)

--- src/main/java/javase03/t01/CrazyLogger.java
+++ src/main/java/javase03/t01/CrazyLogger.java
@@ -4,20 +4,35 @@ import java.io.*;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
+/**
+ * You can use this class for logging messages in your project
+ */
 public class CrazyLogger {
     final private StringBuilder log = new StringBuilder();
 
+    /**
+     * Memorize your message. It uses format dd-mm-yyyy : hh-mm – message.
+     * You can write all messages from the log into a file or into the standard output stream using {@link CrazyLogger#print(PrintStream)} or {@link CrazyLogger#print()}
+     * @param message The message to be logged
+     */
     public void info(CharSequence message){
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-YYYY : hh-mm");
         String formattedDateTime = LocalDateTime.now().format(formatter);
         log.append(formattedDateTime).append(" – ").append(message).append('\n');
     }
 
-    public void print(PrintStream stream) throws IOException{
+    /**
+     * Write all log into a printStream.
+     * @param stream printStream to be written in.
+     */
+    public void print(PrintStream stream){
         stream.print(log.toString());
     }
 
-    public void print() throws IOException{
+    /**
+     * Write all log into the standard output stream.
+     */
+    public void print(){
         this.print(System.out);
     }
 }
--- src/test/java/javase03/t01/CrazyLoggerTest.java
+++ src/test/java/javase03/t01/CrazyLoggerTest.java
@@ -2,11 +2,17 @@ package javase03.t01;
 
 import org.junit.Test;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
+import java.io.PrintStream;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class CrazyLoggerTest {
     @Test
-    public void simpleMessageTest() throws IOException{
+    public void printMessage(){
         CrazyLogger log = new CrazyLogger();
         log.info("crazy message");
         log.print();
