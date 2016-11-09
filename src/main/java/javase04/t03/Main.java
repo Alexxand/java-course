f8205262b63a77a0117bd81ad30591b58eea871e
Alexandr <alexmich@yandex.ru>
2016-11-09 19:26:19 +0300

    Unit 4. Task 3.

---
+++ src/main/java/javase04/t03/Main.java
@@ -0,0 +1,35 @@
+package javase04.t03;
+
+import java.io.*;
+import java.util.Arrays;
+
+import static utils.Utils.getAbsoluteResourcePath;
+
+/**
+ * Created by alexmich on 09.11.16.
+ */
+public class Main {
+    private static final int BUFFER_SIZE = 100;
+    public static void main(String[] args){
+        try (Reader reader = new BufferedReader(
+                new InputStreamReader(
+                        new FileInputStream(getAbsoluteResourcePath("Example.java")),
+                        "utf-8"
+                )
+        );
+        Writer writer = new OutputStreamWriter(
+                new FileOutputStream(getAbsoluteResourcePath("Example.utf-16.java")),
+                "utf-16"
+        )) {
+            char[] buf = new char[BUFFER_SIZE];
+            int nRead;
+            while ((nRead = reader.read(buf)) != -1) {
+                writer.write(buf,0,nRead);
+            }
+
+        } catch(IOException e){
+            e.printStackTrace();
+        }
+
+    }
+}
