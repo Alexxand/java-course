5e1838f56d4c73bf26cb699b92301800f15ae47b
Alexandr <alexmich@yandex.ru>
2016-11-09 16:45:38 +0300

    Unit 4. Task 2. Apache.commons.io library is used to make InputStream from Reader

---
+++ src/main/java/javase04/t02/Main.java
@@ -0,0 +1,27 @@
+package javase04.t02;
+
+import javase04.t01.KeyWordSearcher;
+
+import java.io.*;
+
+import static utils.Utils.getAbsoluteResourcePath;
+
+/**
+ * Created by alexmich on 30.10.16.
+ */
+public class Main {
+    public static void main(String[] args){
+        try(Reader reader = new BufferedReader(
+                new FileReader(getAbsoluteResourcePath("Example.java"))
+            );
+            Writer writer = new FileWriter(
+                    getAbsoluteResourcePath("keyWordsInExample.txt")
+            )) {
+            KeyWordSearcher searcher = KeyWordSearcher.search(reader);
+
+            searcher.print(writer);
+        } catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+}
