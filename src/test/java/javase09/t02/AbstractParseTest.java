c995176f624c8daa1da28e94e88d899d59843358
Alexandr <alexmich@yandex.ru>
2017-01-23 17:50:33 +0300

    Some changes for launche all tests at onse

--- src/test/java/javase09/t02/AbstractParseTest.java
+++ src/test/java/javase09/t02/AbstractParseTest.java
@@ -13,13 +13,16 @@ import static org.junit.Assert.assertEquals;
 public abstract class AbstractParseTest {
     protected static final List<Food> expectedFoodList = new ArrayList<>();;
     protected List<Food> actualFoodList;
+
     @BeforeClass
-    public static void initializePattern(){
-        expectedFoodList.add(new Food(1,"Belgian Waffles",5.95,"two of our famous Belgian Waffles with plenty of real maple syrup",650));
-        expectedFoodList.add(new Food(2,"Strawberry Belgian Waffles",7.95,"light Belgian waffles covered with strawberrys and whipped cream",900));
-        expectedFoodList.add(new Food(3,"Berry-Berry Belgian Waffles",8.95,"light Belgian waffles covered with an assortment of fresh berries and whipped cream",900));
-        expectedFoodList.add(new Food(4,"French Toast",4.50,"thick slices made from our homemade sourdough bread",600));
-        expectedFoodList.add(new Food(5,"Homestyle Breakfast",6.95,"two eggs, bacon or sausage, toast, and our ever-popular hash browns",950));
+    public static void initializePattern() {
+        if (expectedFoodList.isEmpty()) {
+            expectedFoodList.add(new Food(1, "Belgian Waffles", 5.95, "two of our famous Belgian Waffles with plenty of real maple syrup", 650));
+            expectedFoodList.add(new Food(2, "Strawberry Belgian Waffles", 7.95, "light Belgian waffles covered with strawberrys and whipped cream", 900));
+            expectedFoodList.add(new Food(3, "Berry-Berry Belgian Waffles", 8.95, "light Belgian waffles covered with an assortment of fresh berries and whipped cream", 900));
+            expectedFoodList.add(new Food(4, "French Toast", 4.50, "thick slices made from our homemade sourdough bread", 600));
+            expectedFoodList.add(new Food(5, "Homestyle Breakfast", 6.95, "two eggs, bacon or sausage, toast, and our ever-popular hash browns", 950));
+        }
     }
 
     @Test
